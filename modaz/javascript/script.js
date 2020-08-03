dca8317384d9416d00af99809d001b579b40c149
Felipunky <feli20gutierrez@hotmail.com>
2020-08-03 10:54:05 -0500

    Fix home

---
+++ modaz/javascript/script.js
@@ -0,0 +1,14 @@
+/* slider's main JavaScript settings */
+jQuery('#rev-slider1').show().revolution({
+ 
+    sliderLayout: 'auto',
+    gridwidth: 1240,
+    gridheight: 868,
+ 
+    /* Lazy Load options are "all", "smart", "single" and "none" */
+    lazyType: "smart",
+ 
+    /* ****************** */
+    /* settings continued */
+    /* ****************** */
+});
