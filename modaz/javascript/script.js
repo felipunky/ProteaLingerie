12c5a445a71648565b8cb44e33e9c6a4e45d946b
Felipunky <feli20gutierrez@hotmail.com>
2020-08-06 17:14:35 -0500

    Fix isotope

--- modaz/javascript/script.js
+++ modaz/javascript/script.js
@@ -14,21 +14,59 @@ var Success = function () {
 
 $(document).ready( function () {
 
-    var hasItLoaded = document.querySelector( '#iterate' );
+    
+    if( $().isotope ) {
 
-    hasItLoaded.addEventListener( 'load', function() {
+        var hasItLoaded = document.querySelector( '#iterate' );
 
-        Success();
+        hasItLoaded.addEventListener( 'load', function() {
 
-    });
+            Success();
 
-    $('.flat-filter li').on('click',function() {                           
-        var selector = $(this).find("a").attr('data-filter');
-        $('.flat-filter li').removeClass('active');
-        $(this).addClass('active');
-        $container.isotope({ filter: selector });             
-        return false;
-    });
+        });
+
+        $('.flat-filter li').on('click',function() {                           
+            var selector = $(this).find("a").attr('data-filter');
+            $('.flat-filter li').removeClass('active');
+            $(this).addClass('active');
+            $container.isotope({ filter: selector });             
+            return false;
+        });
+
+    }
 
 });
 
+//     var flatIsotope = function() {
+//         if($().isotope) {
+
+//             var $container = $('.isotope-product');
+//             $(window).load( function() {
+//                 $container.imagesLoaded(function(){
+//                     $container.isotope({
+//                         itemSelector: '.product-item',
+//                         transitionDuration: '1s',
+//                         layoutMode: 'fitRows'
+//                     });
+//                 });
+//             });
+
+//             /*$container.imagesLoaded().progress( function() {
+//                 $container.isotope( 'layout' );
+//             });*/
+
+//            /* var $items = $( '.product-item' );
+//             $container.imagesLoaded( function() {
+//                 $container.append( $items ).isotope( 'appended', $items );
+//                 $container.isotope( 'layout' );
+//             }, 200);
+// */
+//             $('.flat-filter li').on('click',function() {                           
+//                 var selector = $(this).find("a").attr('data-filter');
+//                 $('.flat-filter li').removeClass('active');
+//                 $(this).addClass('active');
+//                 $container.isotope({ filter: selector });             
+//                 return false;
+//             });
+//         };
+//     }; 
